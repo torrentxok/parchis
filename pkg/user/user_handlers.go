6d2c606339b1836f46fb418a7cb5e840bf48f4bf
torrentxok <tegaev.1992@mail.ru>
2024-05-15 19:45:04 +0300

    Add user_handlers.go user.go

---
+++ pkg/user/user_handlers.go
@@ -0,0 +1,8 @@
+package user
+
+import "net/http"
+
+func GetUserProfileHandler(w http.ResponseWriter, r *http.Request) {
+	// Реализовать проверку и разделить на 2 функции: для собственного профиля, для другого пользователя
+
+}
